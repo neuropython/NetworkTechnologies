137abdf2e63ca4521677c021452140603a384abf
Damian <damianpietron007@gmail.com>
2024-02-28 10:44:33 +0100

    First Labs

--- src/main/java/com/example/helloworldspring/HelloController.java
+++ src/main/java/com/example/helloworldspring/HelloController.java
@@ -1,9 +1,6 @@
 package com.example.helloworldspring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -15,8 +12,18 @@ public class HelloController {
     }
 
     @GetMapping("/helloSomeone")
-    public String helloSomeone(@RequestParam String name){
-        return "Hello " + name +"!";
+    public String helloSomeone(@RequestParam String name,
+                               @RequestParam String surname){
+        return "Hello " + name + " " + surname + "!";
     }
-
+    @GetMapping("/anotherHello/{name}")
+    public String otherHello(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+    @GetMapping("/add")
+    public String add(@RequestParam Integer number1,
+                      @RequestParam Integer number2) {
+         int sum = number2 + number1;
+         return "" + sum;
+    }
 }
